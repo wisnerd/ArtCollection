bf5064863ae75e50b6edb67e7c1e038274b4ba64
Wisner Dessaline <dessawis10@gmail.com>
2023-11-06 01:44:48 -0500

    store and functinality

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,8 @@ from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 from django.http import HttpResponse
 
@@ -120,5 +122,31 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'grand_total': grand_total,
 
     }
-
     return render(request, 'store/cart.html', context)
+
+@login_required(login_url='login')
+def checkout(request, total=0, quantity=0, cart_items=None):
+    try:
+        tax = 0
+        grand_total = 0
+        # if request.user.is_authenticated:
+        #     cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        # else:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass #just ignore
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'tax'       : tax,
+        'grand_total': grand_total,
+    }
+    return render(request, 'store/checkout.html', context)
